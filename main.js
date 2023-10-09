282b046aa47eda0b764391af93379c58ad9c0237
GuiltySpark05 <rottafalejandro@gmail.com>
2023-10-09 20:22:24 -0300

    carrousel en proceso
    falta estilo

--- main.js
+++ main.js
@@ -1,23 +1,30 @@
-const prevBtn = document.getElementById("prevBtn");
-const nextBtn = document.getElementById("nextBtn");
-const carousel = document.querySelector(".carousel");
-const images = document.querySelectorAll(".carousel img");
+document.addEventListener("DOMContentLoaded", () => {
+    const prevBtn = document.getElementById("prevBtn");
+    const nextBtn = document.getElementById("nextBtn");
+    const carousel = document.querySelector(".carousel");
+    const images = document.querySelectorAll(".carousel img");
 
-let currentIndex = 0;
+    let currentIndex = 0;
 
-prevBtn.addEventListener("click", () => {
-    currentIndex = (currentIndex - 1 + images.length) % images.length;
-    updateCarousel();
+    prevBtn.addEventListener("click", () => {
+        currentIndex = (currentIndex - 1 + images.length) % images.length;
+        actualizarCarrousel();
+    });
+
+    nextBtn.addEventListener("click", () => {
+        currentIndex = (currentIndex + 1) % images.length;
+        actualizarCarrousel();
+    });
+
+    function actualizarCarrousel() {
+        images.forEach((img, index) => {
+            if (index === currentIndex) {
+                img.style.display = "block";
+            } else {
+                img.style.display = "none";
+            }
+        });
+    }
+
+    actualizarCarrousel();
 });
-
-nextBtn.addEventListener("click", () => {
-    currentIndex = (currentIndex + 1) % images.length;
-    updateCarousel();
-});
-
-function updateCarousel() {
-    const offset = -currentIndex * 100;
-    carousel.style.transform = `translateX(${offset}%)`;
-}
-
-updateCarousel();
