d7b7738720787ae8a912150c639ef5dbb300c27a
GuiltySpark05 <rottafalejandro@gmail.com>
2023-10-11 16:45:12 -0300

    11/10/2023

--- main.js
+++ main.js
@@ -2,22 +2,22 @@ document.addEventListener("DOMContentLoaded", () => {
     const prevBtn = document.getElementById("prevBtn");
     const nextBtn = document.getElementById("nextBtn");
     const carousel = document.querySelector(".carousel");
-    const images = document.querySelectorAll(".carousel img");
+    const imagenes = document.querySelectorAll(".carousel img");
 
     let currentIndex = 0;
 
     prevBtn.addEventListener("click", () => {
-        currentIndex = (currentIndex - 1 + images.length) % images.length;
+        currentIndex = (currentIndex - 1 + imagenes.length) % imagenes.length;
         actualizarCarrousel();
     });
 
     nextBtn.addEventListener("click", () => {
-        currentIndex = (currentIndex + 1) % images.length;
+        currentIndex = (currentIndex + 1) % imagenes.length;
         actualizarCarrousel();
     });
 
     function actualizarCarrousel() {
-        images.forEach((img, index) => {
+        imagenes.forEach((img, index) => {
             if (index === currentIndex) {
                 img.style.display = "block";
             } else {
